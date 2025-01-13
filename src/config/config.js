0e2d4f2a5cbb9123bbfad0f25a3f1e9d750bdf91
Victor Mutwiri <68108635+Victor-Mutwiri@users.noreply.github.com>
2025-01-14 00:14:07 +0300

    updated config prod url

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const config = {
       API_BASE_URL: import.meta.env.VITE_API_DEV_URL
     },
     production: {
-      API_BASE_URL: import.meta.env.VITE_API_PROD_URL
+      API_BASE_URL: import.meta.env.VITE_PROD_PROD_API_URL
     }
   };
   
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-/* export default defineConfig({
-  plugins: [react()],
-})
- */
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -20,5 +15,6 @@ export default defineConfig({
     },
     watch: {
       usePolling: true, // Use polling for file watching
-  },
-}});
+    },
+  }
+})
