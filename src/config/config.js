c7d57bcf5ec32be6eba9d25cacfa6ec6a6f69016
Victor Mutwiri <68108635+Victor-Mutwiri@users.noreply.github.com>
2025-03-14 19:38:07 +0300

    modified config

--- src/config/config.js
+++ src/config/config.js
@@ -6,7 +6,7 @@ const config = {
       API_BASE_URL: import.meta.env.VITE_PROD_PROD_API_URL
     }
   };
-  /* Updated config file */
+  /* Updated config files */
   export const API_BASE_URL = import.meta.env.PROD
     ? config.production.API_BASE_URL
     : config.development.API_BASE_URL;
